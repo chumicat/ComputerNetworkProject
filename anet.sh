cec68cea424b72eda1d99d1f8ab719de034c94b8
Chumicat <russell57260620@gmail.com>
2018-06-08 11:03:25 +0800

    fast activate shell
    a shell that you can activate environment in target folder conviently

---
+++ anet.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# First: Change the target folder to your github proj folder in line 8
+# Second: Move this file to home directory
+# Third: When need, use "sh anet.sh" to run (or see reference to run with "./anet.sh")
+# Reference: https://stackoverflow.com/questions/733824/how-to-run-a-shell-script-on-a-unix-console-or-mac-terminal
+
+source net/bin/activate
+cd "Documents/Github/ComputerNetworkProject/"
+jupyter notebook
